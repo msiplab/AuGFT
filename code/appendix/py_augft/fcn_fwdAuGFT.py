a277633e5a09cf24c38d7fe42cfcf2e23f412f81
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2024-12-31 17:42:10 +0900

    Revised the arguments of the functions and optimized the matrix operations in fcn_fwdAuGFT.py and fcn_invAuGFT.py. Added fwdAuGFT and invAuGFT to the return values.

--- code/appendix/py_augft/fcn_fwdAuGFT.py
+++ code/appendix/py_augft/fcn_fwdAuGFT.py
@@ -1,15 +1,11 @@
 import numpy as np
 
-def fcn_fwdAuGFT(x, U, Q, **kwaargs):
-
-    if 'alpha' in kwargs:
-        alpha = kwargs.get('alpha')
-    else:
-        alpha = 1 - 1 / np.sqrt(2)
+def fcn_fwdAuGFT(x, U, Q, alpha=(1-1/np.sqrt(2)) ):
 
     # Forward AuGFT
     I = np.eye(U.shape[0])
     beta = np.sqrt(alpha * (2 - alpha))
-    fwdAuGFT = np.vstack(((I - alpha * np.dot(Q, Q.T)) @ U, beta * Q)).T
-    X = np.dot(fwdAuGFT, x)
-    return X
+    fwdAuGFT = np.hstack(((I - alpha * (Q@Q.T)) @ U, beta * Q)).T
+    X = fwdAuGFT@x
+
+    return X, fwdAuGFT
--- code/appendix/py_augft/fcn_invAuGFT.py
+++ code/appendix/py_augft/fcn_invAuGFT.py
@@ -1,15 +1,11 @@
 import numpy as np
 
-def fcn_invAuGFT(X, U, Q, **kwargs):
-
-    if 'alpha' in kwargs:
-        alpha = kwargs.get('alpha')
-    else:
-        alpha = 1 - 1 / np.sqrt(2)
+def fcn_invAuGFT(X, U, Q, alpha=(1-1/np.sqrt(2))):
     
     # Inverse AuGFT
     I = np.eye(U.shape[0])
     beta = np.sqrt(alpha * (2 - alpha))
-    invAuGFT = np.concatenate(((I - alpha * np.dot(Q, Q.T)) * U, beta * Q), axis=1)
-    x = np.dot(invAuGFT, X)
-    return x, invAuGFT
+    invAuGFT = np.hstack(((I - alpha * (Q@Q.T)) @ U, beta*Q))
+    x = invAuGFT@X
+
+    return (x, invAuGFT)
